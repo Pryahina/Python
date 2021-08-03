abbf81796b182264001fb53f6472a007775bf1aa
Pryahina <88263749+Pryahina@users.noreply.github.com>
2021-08-03 11:12:58 +0300

    Update homework02.py

--- homework02.py
+++ homework02.py
@@ -2,4 +2,4 @@ sec = int(input("Введите время в секундах:"))
 hour = sec // 3600
 minute = sec % 3600 // 60
 sec = sec % 60
-print(f"{hour}:{minute}:{sec}")
+print(f" {hour:>02}:{minute:>02}:{sec:>02}")"Введите время в секундах
